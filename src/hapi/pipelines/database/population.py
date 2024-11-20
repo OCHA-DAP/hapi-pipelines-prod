9b0fb34e5644acf280c0ad0247e9968454dfa5ca
Briar <briar.j.mills@gmail.com>
2024-11-20 12:02:54 -0500

    Rewrite population pipeline using global dataset

--- src/hapi/pipelines/database/population.py
+++ src/hapi/pipelines/database/population.py
@@ -1,26 +1,18 @@
 """Functions specific to the population theme."""
 
-import re
 from logging import getLogger
 from typing import Dict
 
 from hapi_schema.db_population import DBPopulation
+from hdx.utilities.dateparse import parse_date_range
 from sqlalchemy.orm import Session
 
-from ..utilities.parse_tags import (
-    get_gender_and_age_range,
-    get_min_and_max_age,
-)
 from ..utilities.provider_admin_names import get_provider_name
 from . import admins
 from .base_uploader import BaseUploader
 from .metadata import Metadata
 
 logger = getLogger(__name__)
-
-_HXL_PATTERN = re.compile(
-    r"^#population(\+[a-z])*(\+age_(\d+_\d+|\d+_plus))*(\+total)?$"
-)
 
 
 class Population(BaseUploader):
@@ -39,36 +31,50 @@ class Population(BaseUploader):
     def populate(self) -> None:
         logger.info("Populating population table")
         for dataset in self._results.values():
-            time_period_start = dataset["time_period"]["start"]
-            time_period_end = dataset["time_period"]["end"]
-
             for admin_level, admin_results in dataset["results"].items():
                 resource_id = admin_results["hapi_resource_metadata"]["hdx_id"]
                 hxl_tags = admin_results["headers"][1]
                 values = admin_results["values"]
-                for hxl_tag, population_values in zip(hxl_tags, values):
-                    if hxl_tag.startswith("#adm"):
-                        continue
-                    if not _validate_gender_and_age_range_hxl_tag(hxl_tag):
-                        raise ValueError(
-                            f"HXL tag {hxl_tag} not in valid format"
-                        )
-                    gender, age_range = get_gender_and_age_range(
-                        hxl_tag=hxl_tag
+                admin_codes = values[0].keys()
+                for admin_code in admin_codes:
+                    admin2_code = admins.get_admin2_code_based_on_level(
+                        admin_code=admin_code, admin_level=admin_level
                     )
-                    min_age, max_age = get_min_and_max_age(age_range)
-                    for (
-                        admin_code,
-                        population_value,
-                    ) in population_values.items():
-                        admin2_code = admins.get_admin2_code_based_on_level(
-                            admin_code=admin_code, admin_level=admin_level
+                    for irow in range(len(values[0][admin_code])):
+                        gender = values[hxl_tags.index("#gender")][admin_code][
+                            irow
+                        ]
+                        age_range = values[hxl_tags.index("#age+range")][
+                            admin_code
+                        ][irow]
+                        min_age = values[hxl_tags.index("#age+min")][
+                            admin_code
+                        ][irow]
+                        max_age = values[hxl_tags.index("#age+max")][
+                            admin_code
+                        ][irow]
+                        population = values[hxl_tags.index("#population")][
+                            admin_code
+                        ][irow]
+                        reference_year = values[hxl_tags.index("#date+year")][
+                            admin_code
+                        ][irow]
+                        time_period_range = parse_date_range(
+                            reference_year, "%Y"
                         )
                         provider_admin1_name = get_provider_name(
-                            values, "#adm1+name", hxl_tags, admin_code
+                            values,
+                            "#adm1+name",
+                            hxl_tags,
+                            admin_code,
+                            irow,
                         )
                         provider_admin2_name = get_provider_name(
-                            values, "#adm2+name", hxl_tags, admin_code
+                            values,
+                            "#adm2+name",
+                            hxl_tags,
+                            admin_code,
+                            irow,
                         )
                         population_row = DBPopulation(
                             resource_hdx_id=resource_id,
@@ -79,25 +85,10 @@ class Population(BaseUploader):
                             age_range=age_range,
                             min_age=min_age,
                             max_age=max_age,
-                            population=int(population_value),
-                            reference_period_start=time_period_start,
-                            reference_period_end=time_period_end,
+                            population=int(population),
+                            reference_period_start=time_period_range[0],
+                            reference_period_end=time_period_range[1],
                         )
 
                         self._session.add(population_row)
         self._session.commit()
-
-
-def _validate_gender_and_age_range_hxl_tag(hxl_tag: str) -> bool:
-    """Validate HXL tags
-
-    Assume they have the form:
-        #population+total
-        #population+f+total
-        #population+age_5_12+total
-        #population+age_80_plus+total
-        #population+f+age_5_12
-        #population+f+age_80_plus
-    """
-    # TODO: add tests for this (HAPI-159)
-    return bool(_HXL_PATTERN.match(hxl_tag))
